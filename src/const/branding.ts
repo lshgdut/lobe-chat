fedffea599c6e31e13ca7bed12449e0ae4876be0
wenhua <lshgdut@qq.com>
2025-04-20 07:35:34 +0000

    chore: 修改公司标识

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'QINGLING Copilot Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = '清岭数智助手';
+export const BRANDING_LOGO_URL = '/qingling-icon.png?v=1';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '深圳市清岭技术有限公司';
 
 export const BRANDING_URL = {
   help: undefined,
