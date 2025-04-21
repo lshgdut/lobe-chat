6ef4d4433b4088ee172a8c48567fe059803bd474
wenhua <lshgdut@qq.com>
2025-04-21 15:50:00 +0000

    chore: 隐藏更新日志

--- src/app/[variants]/(main)/settings/about/features/Version.tsx
+++ src/app/[variants]/(main)/settings/about/features/Version.tsx
@@ -58,9 +58,10 @@ const Version = memo<{ mobile?: boolean }>(({ mobile }) => {
         </Flexbox>
       </Flexbox>
       <Flexbox flex={mobile ? 1 : undefined} gap={8} horizontal>
-        <Link href={CHANGELOG_URL} style={{ flex: 1 }} target={'_blank'}>
+        {/* NOTE(lsh): 隐藏更新日志 */}
+        {/* <Link href={CHANGELOG_URL} style={{ flex: 1 }} target={'_blank'}>
           <Button block={mobile}>{t('changelog')}</Button>
-        </Link>
+        </Link> */}
         {hasNewVersion && (
           <Link href={MANUAL_UPGRADE_URL} style={{ flex: 1 }} target={'_blank'}>
             <Button block={mobile} type={'primary'}>
