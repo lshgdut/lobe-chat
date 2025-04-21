0dff633650fa861ea5da8d42719267372b66b394
wenhua <lshgdut@qq.com>
2025-04-21 15:51:03 +0000

    chore: 更新联系邮箱

--- src/const/url.ts
+++ src/const/url.ts
@@ -61,8 +61,8 @@ export const SESSION_CHAT_URL = (id: string = INBOX_SESSION_ID, mobile?: boolean
 export const imageUrl = (filename: string) => withBasePath(`/images/${filename}`);
 
 export const LOBE_URL_IMPORT_NAME = 'settings';
-export const EMAIL_SUPPORT = 'support@lobehub.com';
-export const EMAIL_BUSINESS = 'hello@lobehub.com';
+export const EMAIL_SUPPORT = 'kf@qingling-ai.com';
+export const EMAIL_BUSINESS = 'kf@qingling-ai.com';
 
 export const MEDIDUM = 'https://medium.com/@lobehub';
 export const X = 'https://x.com/lobehub';
