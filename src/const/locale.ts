f484cbe3477142b7be28e870dae99693f309c4f4
wenhua <lshgdut@qq.com>
2025-04-21 14:42:54 +0000

    chore: 默认语言调整为zh-CN

--- src/const/locale.ts
+++ src/const/locale.ts
@@ -1,7 +1,7 @@
 import { supportLocales } from '@/locales/resources';
 
-export const DEFAULT_LANG = 'en-US';
-export const LOBE_LOCALE_COOKIE = 'LOBE_LOCALE';
+export const DEFAULT_LANG = 'zh-CN';
+export const LOBE_LOCALE_COOKIE = 'QL_LOCALE';
 
 /**
  * Check if the language is supported
