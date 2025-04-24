66e38847cc90d7c0e4e4e3a73739e7ecbb19fb05
wenhua <lshgdut@qq.com>
2025-04-24 14:41:17 +0000

    chore: 隐藏系统设置的密码和重置配置

--- src/app/[variants]/(main)/settings/common/index.tsx
+++ src/app/[variants]/(main)/settings/common/index.tsx
@@ -1,11 +1,13 @@
 import Appearance from './features/Appearance';
 import ChatAppearance from './features/ChatAppearance';
+import { serverFeatureFlags } from '@/config/featureFlags';
 import Common from './features/Common';
 
 const Page = () => {
+  const qinglingCustomized = serverFeatureFlags().qinglingCustomized
   return (
     <>
-      <Common />
+      {!qinglingCustomized && <Common />}
       <Appearance />
       <ChatAppearance />
     </>
