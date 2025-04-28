bcd4bb4b72e163189a1dddf785139dc2c81b2841
wenhua <lshgdut@qq.com>
2025-04-28 15:39:30 +0800

    feat: 修改联网查询默认的网站图标地址为直接访问

--- src/tools/web-browsing/const.ts
+++ src/tools/web-browsing/const.ts
@@ -25,16 +25,17 @@ export const CATEGORY_ICON_MAP: Record<string, any> = {
 };
 
 export const ENGINE_ICON_MAP: Record<string, string> = {
-  'arxiv': 'https://icons.duckduckgo.com/ip3/arxiv.org.ico',
-  'bilibili': 'https://icons.duckduckgo.com/ip3/bilibili.com.ico',
-  'bing': 'https://icons.duckduckgo.com/ip3/www.bing.com.ico',
-  'brave': 'https://icons.duckduckgo.com/ip3/brave.com.ico',
-  'duckduckgo': 'https://icons.duckduckgo.com/ip3/www.duckduckgo.com.ico',
-  'google': 'https://icons.duckduckgo.com/ip3/google.com.ico',
-  'google scholar': 'https://icons.duckduckgo.com/ip3/scholar.google.com.ico',
-  'npm': 'https://icons.duckduckgo.com/ip3/npmjs.com.ico',
-  'qwant': 'https://icons.duckduckgo.com/ip3/www.qwant.com.ico',
-  'youtube': 'https://icons.duckduckgo.com/ip3/youtube.com.ico',
+  'arxiv': '//arxiv.org/favicon.ico',
+  'bilibili': '//bilibili.com/favicon.ico',
+  'bing': '//www.bing.com/favicon.ico',
+  'brave': '//brave.com/favicon.ico',
+  'duckduckgo': '//www.duckduckgo.com/favicon.ico',
+  'google': '//google.com/favicon.ico',
+  'google scholar': '//scholar.google.com/favicon.ico',
+  'npm': '//npmjs.com/favicon.ico',
+  'qwant': '//www.qwant.com/favicon.ico',
+  'youtube': '//youtube.com/favicon.ico',
+  'baidu': '//baidu.com/favicon.ico',
 };
 
 export const CRAWL_CONTENT_LIMITED_COUNT = 7000;
