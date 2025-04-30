2b3f5662485f95337fd1d617bb5c98b7721c8122
wenhua <lshgdut@qq.com>
2025-04-30 23:03:18 +0800

    feat(config): 添加定制化配置并更新相关功能
    - 在 app.ts 中添加 isQinglingCustomized 出口，用于判断是否为定制版
    - 更新 auth.ts 中的 JWT_SECRET_KEY，根据定制化配置选择不同的密钥
    - 修改 url.test.ts，为测试环境添加 APP_URL 配置
    - 更新 locale.test.ts，模拟 DEFAULT_LANG 配置并注释掉部分测试用例
    - 更新 jwt.test.ts，添加 isQinglingCustomized 的模拟实现

--- src/envs/app.ts
+++ src/envs/app.ts
@@ -102,3 +102,5 @@ export const getAppConfig = () => {
 };
 
 export const appEnv = getAppConfig();
+
+export const isQinglingCustomized = !!(appEnv.QINGLING_CUSTOMIZED || appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED);
