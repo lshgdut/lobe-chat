1ebccb9e49dbeddb1e947d8d9eef0dd0615c4213
wenhua <lshgdut@qq.com>
2025-05-07 11:53:04 +0800

    build(deps): 降级 pnpm 版本并添加 'use client' 指令
    - 将 pnpm 版本从 10.4.1 降级到 9.15.9
    - 在设置页面的公共组件中添加 'use client' 指令

--- src/app/[variants]/(main)/settings/common/index.tsx
+++ src/app/[variants]/(main)/settings/common/index.tsx
@@ -1,10 +1,13 @@
+'use client';
+
 import Appearance from './features/Appearance';
 import ChatAppearance from './features/ChatAppearance';
 import { useServerConfigStore } from '@/store/serverConfig';
+
 import Common from './features/Common';
 
 const Page = () => {
-  const { isQinglingCustomized } = useServerConfigStore((s)=>s.serverConfig)
+  const { isQinglingCustomized } = useServerConfigStore((s) => s.serverConfig);
   return (
     <>
       {!isQinglingCustomized && <Common />}
